37a66a29e1c801a8868bcf4a97385b8c92d02799
qiuscraft <2811883713@qq.com>
2024-07-25 20:14:38 +0800

    修改图标和标题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   app: {
     // head
     head: {
-      title: 'Element Plus + Nuxt 3',
+      title: 'Poker Timer',
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
