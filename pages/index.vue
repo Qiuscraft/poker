ec76e99b60484b943d1576c77a12bce5d95c5348
qiuscraft <2811883713@qq.com>
2024-07-27 19:48:59 +0800

    新增功能：自定义盲注结构

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="center-container">
-    <div>{{ timeString }}</div>
-    <div>盲注: {{ smallBlind }} / {{ bigBlind }}</div>
-    <div class="progress-container">
-      <el-progress :percentage="progressPercentage" :show-text="false" />
+  <div>
+    <div class="center-container">
+      <div>{{ timeString }}</div>
+      <div>盲注: {{ smallBlind }} / {{ bigBlind }}</div>
+      <div class="progress-container">
+        <el-progress :percentage="progressPercentage" :show-text="false" />
+      </div>
+      <div v-if="started && !paused" class="button-container">
+        <el-button type="primary" plain disabled>开始计时</el-button>
+        <el-button type="warning" plain @click="pause()">暂停计时</el-button>
+        <el-button type="danger" plain @click="stop()">停止计时</el-button>
+      </div>
+      <div v-else-if="started && paused" class="button-container">
+        <el-button type="primary" plain disabled>开始计时</el-button>
+        <el-button type="warning" plain @click="resume()">继续计时</el-button>
+        <el-button type="danger" plain @click="stop()">停止计时</el-button>
+      </div>
+      <div v-else class="button-container">
+        <el-button type="primary" plain @click="start()">开始计时</el-button>
+        <el-button type="warning" plain disabled>暂停计时</el-button>
+        <el-button type="danger" plain disabled>停止计时</el-button>
+      </div>
     </div>
-    <div v-if="started && !paused" class="button-container">
-      <el-button type="primary" plain disabled>开始计时</el-button>
-      <el-button type="warning" plain @click="pause()">暂停计时</el-button>
-      <el-button type="danger" plain @click="stop()">停止计时</el-button>
-    </div>
-    <div v-else-if="started && paused" class="button-container">
-      <el-button type="primary" plain disabled>开始计时</el-button>
-      <el-button type="warning" plain @click="resume()">继续计时</el-button>
-      <el-button type="danger" plain @click="stop()">停止计时</el-button>
-    </div>
-    <div v-else class="button-container">
-      <el-button type="primary" plain @click="start()">开始计时</el-button>
-      <el-button type="warning" plain disabled>暂停计时</el-button>
-      <el-button type="danger" plain disabled>停止计时</el-button>
+    <div class="config">
+      涨盲时间：（单位：分钟）<el-input-number v-model="rising_time" :min="1" :max="1440" />
+      小盲列表：（大盲会自动计算）<el-input
+      v-model="sb_list"
+      style="width: 240px"
+      autosize
+      type="textarea"
+      />
     </div>
   </div>
 </template>
@@ -28,42 +39,36 @@ const started = ref(false)
 const paused = ref(false)
 const time = ref(0)
 let id: NodeJS.Timeout
+const rising_time = ref(12)
+const sb_list = ref('25\n50\n100\n200\n400\n800\n1600')
 
-const blindLevels = [
-  { smallBlind: 10, bigBlind: 20 },
-  { smallBlind: 15, bigBlind: 30 },
-  { smallBlind: 20, bigBlind: 40 },
-  { smallBlind: 25, bigBlind: 50 },
-  { smallBlind: 30, bigBlind: 60 },
-  { smallBlind: 40, bigBlind: 80 },
-  { smallBlind: 50, bigBlind: 100 },
-  { smallBlind: 60, bigBlind: 120 },
-  { smallBlind: 80, bigBlind: 160 },
-  { smallBlind: 100, bigBlind: 200 },
-  { smallBlind: 120, bigBlind: 240 },
-  { smallBlind: 150, bigBlind: 300 },
-  { smallBlind: 200, bigBlind: 400 },
-  { smallBlind: 250, bigBlind: 500 },
-  { smallBlind: 300, bigBlind: 600 },
-  { smallBlind: 400, bigBlind: 800 },
-  { smallBlind: 500, bigBlind: 1000 },
-  { smallBlind: 600, bigBlind: 1200 }
-]
+const blindLevels = computed(() => {
+  return sb_list.value.split('\n').map((sb) => {
+    return {
+      smallBlind: parseInt(sb),
+      bigBlind: parseInt(sb) * 2
+    }
+  })
+})
 
 const currentLevel = computed(() => {
-  return Math.floor(time.value / 360)
+  return Math.floor(time.value / (rising_time.value * 60))
 })
 
 const smallBlind = computed(() => {
-  return blindLevels[currentLevel.value].smallBlind
+  if (currentLevel.value >= blindLevels.value.length) 
+    return blindLevels.value[blindLevels.value.length - 1].smallBlind
+  return blindLevels.value[currentLevel.value].smallBlind
 })
 
 const bigBlind = computed(() => {
-  return blindLevels[currentLevel.value].bigBlind
+  if (currentLevel.value >= blindLevels.value.length) 
+    return blindLevels.value[blindLevels.value.length - 1].bigBlind
+  return blindLevels.value[currentLevel.value].bigBlind
 })
 
 const progressPercentage = computed(() => {
-  return (time.value % 360) / 360 * 100
+  return (time.value % (rising_time.value * 60)) / (rising_time.value * 60) * 100
 })
 
 function start() {
@@ -83,7 +88,7 @@ function pause() {
 function resume() {
   paused.value = false
   id = setInterval(() => {
-    time.value += 10
+    time.value += 0.01
   }, 10)
 }
 
@@ -119,5 +124,12 @@ const timeString = computed(() => {
 .progress-container {
   width: 18rem;
 }
-
+.config {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+}
 </style>
